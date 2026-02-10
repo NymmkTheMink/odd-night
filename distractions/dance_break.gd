f058f9a76979767f5644d3ee8c751b25de885963
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2026-02-10 10:08:58 +0100

    changed how target key is showed (wip)
    From only showing target key to showing all keys and highlighting target key.

--- distractions/dance_break.gd
+++ distractions/dance_break.gd
@@ -1,21 +1,28 @@
 extends Control
 
-@onready var target = get_node("Target")
-
+var target = 1
 var score = 0
 var target_score
 
+var active_target_color = Color(0,1,0,1)
+var inactive_target_color = Color(1,1,1,0.5)
+
+@onready var target_1 = %Target1
+@onready var target_2 = %Target2
+@onready var target_3 = %Target3
+@onready var target_4 = %Target4
+
 var choices = {
-	"left": "res://assets/kenney_input-prompts/Keyboard & Mouse/Default/keyboard_arrow_left.png",
-	"right": "res://assets/kenney_input-prompts/Keyboard & Mouse/Default/keyboard_arrow_right.png",
-	"up": "res://assets/kenney_input-prompts/Keyboard & Mouse/Default/keyboard_arrow_up.png",
-	"down": "res://assets/kenney_input-prompts/Keyboard & Mouse/Default/keyboard_arrow_down.png",
+	"left": 3,
+	"right": 4,
+	"up": 2,
+	"down": 1,
 }
 
 
 func _ready() -> void:
 	target_score = random_target_score()
-	print(target_score)
+	_set_target()
 
 
 func random_target_score():
@@ -31,31 +38,45 @@ func _process(delta: float) -> void:
 		get_tree().paused = false
 	
 	if Input.is_action_just_pressed("left"):
-		if "left" in target.texture.resource_path:
+		if target == 3:
 			$Correct.play()
 			score += 1
-			_set_target_tex()
+			target_3.modulate = inactive_target_color
+			_set_target()
 	elif Input.is_action_just_pressed("right"):
-		if "right" in target.texture.resource_path:
+		if target == 4:
 			$Correct.play()
 			score += 1
-			_set_target_tex()
+			target_4.modulate = inactive_target_color
+			_set_target()
 	elif Input.is_action_just_pressed("up"):
-		if "up" in target.texture.resource_path:
+		if target == 2:
 			$Correct.play()
 			score += 1
-			_set_target_tex()
+			target_2.modulate = inactive_target_color
+			_set_target()
 	elif Input.is_action_just_pressed("down"):
-		if "down" in target.texture.resource_path:
+		if target == 1:
 			$Correct.play()
 			score += 1
-			_set_target_tex()
+			target_1.modulate = inactive_target_color
+			_set_target()
 
 
-func _set_target_tex():
+func _set_target():
 	randomize()
 	var size = choices.size()
 	var random_key = choices.keys()[randi() % size]
-	var btn_texture = choices[random_key]
+	var target_number = choices[random_key]
 	randomize()
-	target.texture = ResourceLoader.load(btn_texture)
+	
+	match target_number:
+		1:
+			target_1.modulate = active_target_color
+		2:
+			target_2.modulate = active_target_color
+		3:
+			target_3.modulate = active_target_color
+		4:
+			target_4.modulate = active_target_color
+	target = target_number
